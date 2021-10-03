b8929fd0e2e951fc87a49e9b1d3b6d1dfa625889
FriedCactus <76611552+FriedCactus@users.noreply.github.com>
2021-10-03 13:59:01 +0400

    Fix transition

--- src/components/GlobalStyles/GlobalStyles.jsx
+++ src/components/GlobalStyles/GlobalStyles.jsx
@@ -8,7 +8,6 @@ const GlobalStyles = createGlobalStyle`
 
         background-color: ${(props) => props.theme.mainColor};
         color: ${(props) => props.theme.secondaryColor};
-        transition: all 0.3s;
     }
 
     *,
